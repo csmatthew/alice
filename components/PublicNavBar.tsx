b73655ee8bd6577d741d0488700e7af4d350b720
Christopher S. Matthew <christopher.s.matthew@gmail.com>
2025-06-25 20:07:50 +0100

    add logo to navbar

--- components/PublicNavBar.tsx
+++ components/PublicNavBar.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function PublicNavBar() {
     return (
         <nav
@@ -6,8 +9,17 @@ export default function PublicNavBar() {
         items-center fixed 
         z-50 w-full h-28 
         bh-gray-300 px-10 
-        gap-4 shadow-2xl
-        "
-        ></nav>
+        gap-4 shadow-2xl">
+            {/* Logo */}
+            <Link href="/login" className="flex items-center gap-1 hover:scale-150 duration-500">
+            <Image
+                src="/assets/logo.svg"
+                width={60}
+                height={60}
+                alt="alice logo"
+            />
+            </Link>
+
+        </nav>
     );
 }
