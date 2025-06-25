ac5dcba554c862b54ed67a701fceae5ad104eb72
Christopher S. Matthew <christopher.s.matthew@gmail.com>
2025-06-25 19:52:56 +0100

    feat: add book view and public navbar

---
+++ components/PublicNavBar.tsx
@@ -0,0 +1,13 @@
+export default function PublicNavBar() {
+    return (
+        <nav
+            className="
+        flex justify-between 
+        items-center fixed 
+        z-50 w-full h-28 
+        bh-gray-300 px-10 
+        gap-4 shadow-2xl
+        "
+        ></nav>
+    );
+}
